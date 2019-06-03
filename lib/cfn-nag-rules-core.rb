c7a1f7991b376ef957fb89b105a4b8dce120035f
Jesse R. Adams <jesse.adams@stelligent.com>
2019-06-03 09:01:35 -0700

    Fixing pipeline

--- lib/cfn-nag-rules-core.rb
+++ lib/cfn-nag-rules-core.rb
@@ -2,6 +2,5 @@
 
 gem_path = Gem.loaded_specs['cfn-nag-rules-core'].full_gem_path
 Dir.glob("#{gem_path}/lib/cfn-nag-rules-core/**/*.rb").each do |rule|
-  puts "Loading rule: #{rule}"
   require rule
 end
